c2b1425ee9dcda305824a96f4be495dd3a6ce750
James Chen <jianhua.chen@cocos2d-x.org>
2015-09-15 09:55:45 +0800

    Updates init.el

--- init.el
+++ init.el
@@ -198,6 +198,7 @@ user code."
  This function is called at the very end of Spacemacs initialization after
 layers configuration. You are free to put any user code."
 
+  (setq default-tab-width 4)
   (global-linum-mode 1) ; always show line numbers
 
   (setq powerline-default-separator 'arrow)
@@ -335,6 +336,78 @@ layers configuration. You are free to put any user code."
     '(progn
        (setq lua-indent-level 4)
        ))
+
+;;   (defun my-delete-word (arg)
+;;     "Delete characters forward until encountering the end of a word.
+;; With argument, do this that many times.
+;; This command does not push text to `kill-ring'."
+;;     (interactive "p")
+;;     (delete-region
+;;      (point)
+;;      (progn
+;;        (forward-word arg)
+;;        (point))))
+
+;;   (defun my-backward-delete-word (arg)
+;;     "Delete characters backward until encountering the beginning of a word.
+;; With argument, do this that many times.
+;; This command does not push text to `kill-ring'."
+;;     (interactive "p")
+;;     (my-delete-word (- arg)))
+
+;;   (defun my-delete-line ()
+;;     "Delete text from current position to end of line char.
+;; This command does not push text to `kill-ring'."
+;;     (interactive)
+;;     (delete-region
+;;      (point)
+;;      (progn (end-of-line 1) (point)))
+;;     (delete-char 1))
+
+;;   (defun my-delete-line-backward ()
+;;     "Delete text between the beginning of the line to the cursor position.
+;; This command does not push text to `kill-ring'."
+;;     (interactive)
+;;     (let (p1 p2)
+;;       (setq p1 (point))
+;;       (beginning-of-line 1)
+;;       (setq p2 (point))
+;;       (delete-region p1 p2)))
+
+  (defun my-copy-line (&optional arg)
+    "Save current line into Kill-Ring without mark the line"
+    (interactive "P")
+    (let ((beg (line-beginning-position)) 
+          (end (line-end-position arg)))
+      (copy-region-as-kill beg end))
+    )
+
+
+  (defun my-copy-word (&optional arg)
+    "Copy words at point"
+    (interactive "P")
+    (let ((beg (progn (if (looking-back "[a-zA-Z0-9]" 1) (backward-word 1)) (point))) 
+          (end (progn (forward-word arg) (point))))
+      (copy-region-as-kill beg end))
+    )
+
+  (defun my-delete-word (&optional arg)
+    "Delete words at point"
+    (interactive "P")
+    (let ((beg (progn (if (looking-back "[a-zA-Z0-9]" 1) (backward-word 1)) (point))) 
+          (end (progn (forward-word arg) (point))))
+      (kill-region beg end))
+    )
+
+  (global-set-key (kbd "C-S-d") 'my-delete-word)
+
+  (defun my-copy-paragraph (&optional arg)
+    "Copy paragraphes at point"
+    (interactive "P")
+    (let ((beg (progn (backward-paragraph 1) (point))) 
+          (end (progn (forward-paragraph arg) (point))))
+      (copy-region-as-kill beg end))
+    )
 
   )
 
